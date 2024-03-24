8c22fcda6c2a86b587170d7728ebf3b96491be65
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2024-03-24 12:12:27 +0100

    switch to the user model instead of user

--- src/main/java/io/github/hrashk/task/tracker/tasks/web/TaskMapper.java
+++ src/main/java/io/github/hrashk/task/tracker/tasks/web/TaskMapper.java
@@ -1,10 +1,11 @@
 package io.github.hrashk.task.tracker.tasks.web;
 
 import io.github.hrashk.task.tracker.tasks.Task;
+import io.github.hrashk.task.tracker.users.web.UserMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, uses = UserMapper.class)
 public abstract class TaskMapper {
     public abstract TaskModel map(Task task);
 
--- src/main/java/io/github/hrashk/task/tracker/tasks/web/TaskModel.java
+++ src/main/java/io/github/hrashk/task/tracker/tasks/web/TaskModel.java
@@ -1,7 +1,7 @@
 package io.github.hrashk.task.tracker.tasks.web;
 
 import io.github.hrashk.task.tracker.tasks.TaskStatus;
-import io.github.hrashk.task.tracker.users.User;
+import io.github.hrashk.task.tracker.users.web.UserModel;
 
 import java.time.Instant;
 import java.util.Set;
@@ -13,8 +13,8 @@ public record TaskModel(
         Instant createdAt,
         Instant updatedAt,
         TaskStatus status,
-        User author,
-        User assignee,
-        Set<User> observers
+        UserModel author,
+        UserModel assignee,
+        Set<UserModel> observers
 ) {
 }
